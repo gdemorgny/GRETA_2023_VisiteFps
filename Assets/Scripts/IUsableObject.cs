9ef005494ba29895a0da3887fcd4f05c4ae94266
Gilbert DEMORGNY <gdemorgny@dreamtogo.fr>
2023-02-01 11:06:12 +0100

    add interfaces

---
+++ Assets/Scripts/IUsableObject.cs
@@ -0,0 +1,4 @@
+public interface IUsableObject 
+{
+    public void UseObject() { }
+}
---
+++ Assets/Scripts/MaterialChangeColor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChangeColor : MonoBehaviour, IUsableObject
+{
+    [SerializeField] private Color[] _colors;
+    private MeshRenderer _renderer;
+    private int _actualColor = -1;
+    private void Start()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+        ChangeColor();
+    }
+
+    public void UseObject() { ChangeColor(); }
+
+    private void ChangeColor()
+    {
+        _actualColor++;
+        if(_actualColor >= _colors.Length)
+        {
+            _actualColor = 0;
+        }
+        _renderer.material.SetColor("_BaseColor",_colors[_actualColor]); 
+    }
+
+}
